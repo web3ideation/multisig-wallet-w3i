75754da0c03c462630db9251ae4583f27ba49598
WolfgangNI <wolfgang.NFTideation@gmail.com>
2024-08-23 05:14:53 -0600

    fixed openzeppelin import

--- src/multisigWallet.sol
+++ src/multisigWallet.sol
@@ -5,11 +5,11 @@
 // See the LICENSE file for more details.
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
 
 /**
  * @title MultiSigWallet
