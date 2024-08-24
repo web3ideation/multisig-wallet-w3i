cc065ae54c64e780cf96abf3cd010a6484fc42d7
WolfgangNI <wolfgang.NFTideation@gmail.com>
2024-08-24 07:02:33 -0600

    refined deploy script

--- script/DeployMultisigWallet.s.sol
+++ script/DeployMultisigWallet.s.sol
@@ -9,7 +9,11 @@ contract DeployMultisigWallet is Script {
     function run() external returns (MultisigWallet) {
         vm.startBroadcast();
 
-        MultisigWallet multisigWallet = new MultisigWallet();
+        address[] memory owners = new address[](2);
+        owners[0] = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+        owners[1] = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+
+        MultisigWallet multisigWallet = new MultisigWallet(owners);
 
         vm.stopBroadcast();
         return multisigWallet;
