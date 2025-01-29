e7de24e5d2496b33e81765f1db994cdce9ccd1ef
WolfgangNI <wolfgang.NFTideation@gmail.com>
2025-01-29 12:44:23 -0600

    updated owner addresses

--- script/DeployMultisigWalletMainnet.s.sol
+++ script/DeployMultisigWalletMainnet.s.sol
@@ -12,8 +12,8 @@ contract DeployMultisigWalletMainnet is Script {
 
         // Specify the multisig owners
         address[] memory owners = new address[](3);
-        owners[0] = 0x0000000000000000000000000000000000000000; // Stefan
-        owners[1] = 0x759941ECB2B2961566C94e4dB53ae3EeC35F980c; // Wolfi
+        owners[0] = 0xd5A4Ed9d14bc273ce995B4E7E8fa0a21E59F8F3b; // Stefan
+        owners[1] = 0xBae2957B8c6CC7D39b7fDF5e82Cf8C467B86Be40; // Wolfi
         owners[2] = 0x5a7c04218942c1c9baED35289A9b3eDfEd6F216F; // Niklas
 
         MultisigWallet multisigWallet = new MultisigWallet(owners);
